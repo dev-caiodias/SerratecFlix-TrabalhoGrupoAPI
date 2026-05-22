ab6ebc15ab66577964898b626c4c48189d1ab185
dev-caiodias <dev.caiodias19@gmail.com>
2026-05-22 16:00:37 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/SerratecFlix/trabalhoApi/TrabalhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SerratecFlix.trabalhoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
