c2d87e217e227da4cf092c295c94d8a319df44f0
dev-caiodias <dev.caiodias19@gmail.com>
2026-05-23 08:56:57 -0300

    FEAT: DTOs request e response de USUARIO

--- src/main/java/com/SerratecFlix/trabalhoApi/Dto/Request/UsuarioDTORequest.java
+++ src/main/java/com/SerratecFlix/trabalhoApi/Dto/Request/UsuarioDTORequest.java
@@ -1,4 +1,26 @@
 package com.SerratecFlix.trabalhoApi.Dto.Request;
 
-public record UsuarioDTORequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioDTORequest {
+    @NotBlank
+    private String nome;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String userName;
+    @NotBlank
+    @Size(min = 8)
+    private String senha;
 }
