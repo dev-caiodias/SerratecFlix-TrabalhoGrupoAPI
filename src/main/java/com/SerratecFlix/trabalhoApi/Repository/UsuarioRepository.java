fafce86d898c53a0b0ab185b6af543337a1c8c79
dev-caiodias <dev.caiodias19@gmail.com>
2026-05-22 21:30:32 -0300

    feat: Entity e repository de USUARIO

--- src/main/java/com/SerratecFlix/trabalhoApi/Repository/UsuarioRepository.java
+++ src/main/java/com/SerratecFlix/trabalhoApi/Repository/UsuarioRepository.java
@@ -1,7 +1,15 @@
 package com.SerratecFlix.trabalhoApi.Repository;
 
+import com.SerratecFlix.trabalhoApi.Domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UsuarioRepository {
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findByUsername(String username);
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
