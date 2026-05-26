33f3db2ff91744921a63c95c6fa4c21f5613a84a
dev-caiodias <dev.caiodias19@gmail.com>
2026-05-26 17:05:45 -0300

    feat: implementando JPQL no Filme e Serie repository

--- src/main/java/com/SerratecFlix/trabalhoApi/Repository/SerieRepository.java
+++ src/main/java/com/SerratecFlix/trabalhoApi/Repository/SerieRepository.java
@@ -1,10 +1,13 @@
 package com.SerratecFlix.trabalhoApi.Repository;
 
 import com.SerratecFlix.trabalhoApi.Domain.Serie;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
@@ -17,4 +20,14 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
 
     @Query(value = "SELECT * FROM tb_serie WHERE temporadas >= :temporadasMinimas", nativeQuery = true)
     List<Serie> buscarPorTemporadasMinimas(@Param("temporadasMinimas") Integer temporadasMinimas);
+
+    @Query("""
+    SELECT DISTINCT s FROM Serie s
+    JOIN s.categorias c
+    WHERE (:categoria IS NULL OR LOWER(c.nome) = LOWER(:categoria))
+    AND (:notaMinima IS NULL OR s.notaMedia >= :notaMinima)
+    """)
+    Page<Serie> filtrarCatalogo(@Param("categoria") String categoria,
+                                @Param("notaMinima") Double notaMinima,
+                                Pageable pageable);
 }
