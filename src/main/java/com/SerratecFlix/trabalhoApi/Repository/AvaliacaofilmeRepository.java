b81f246c19f30ccdf3a0695188bf697fae07d8e7
dev-caiodias <dev.caiodias19@gmail.com>
2026-05-26 18:31:43 -0300

    merge caio

--- src/main/java/com/SerratecFlix/trabalhoApi/Repository/AvaliacaofilmeRepository.java
+++ src/main/java/com/SerratecFlix/trabalhoApi/Repository/AvaliacaofilmeRepository.java
@@ -1,23 +1,13 @@
 package com.SerratecFlix.trabalhoApi.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.SerratecFlix.trabalhoApi.Domain.AvaliacaoFilme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.SerratecFlix.trabalhoApi.Domain.AvaliacaoFilme;
-
-public interface AvaliacaofilmeRepository extends JpaRepository<AvaliacaoFilme, Long> {
-
-    List<AvaliacaoFilme> findByFilmeId(Long filmeId);
-
-    List<AvaliacaoFilme> findByUsuarioId(Long usuarioId);
-
-    @Query("SELECT AVG(a.nota) FROM AvaliacaoFilme a WHERE a.filme.id = :filmeId")
-    Optional<Double> calcularMedia(@Param("filmeId") Long filmeId);
-
-    @Query("SELECT AVG(a.nota) FROM AvaliacaoFilme a WHERE a.filme.id = :filmeId")
+public interface AvaliacaoFilmeRepository extends JpaRepository<AvaliacaoFilme, Long> {
+    @Query("SELECT AVG(a.id) FROM AvaliacaoFilme a WHERE a.filme.id = :filmeId") 
     Double calcularMediaPorFilmeId(@Param("filmeId") Long filmeId);
 }
+
+//Classe temporaria pra testes
