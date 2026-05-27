f4f79461b08d6673779662be44d715d4347616ee
Mario Praun <mpraun.dev@gmail.com>
2026-05-27 19:20:13 -0300

    refactor: organiza estrutura do SerieRepository

--- src/main/java/com/SerratecFlix/trabalhoApi/Repository/SerieRepository.java
+++ src/main/java/com/SerratecFlix/trabalhoApi/Repository/SerieRepository.java
@@ -1,13 +1,11 @@
 package com.SerratecFlix.trabalhoApi.Repository;
 
 import com.SerratecFlix.trabalhoApi.Domain.Serie;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Repository
