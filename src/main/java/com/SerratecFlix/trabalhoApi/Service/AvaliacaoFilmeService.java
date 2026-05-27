3740a98f8ab2b0cdd4794a8508e90d3e99fc43fb
dev-caiodias <dev.caiodias19@gmail.com>
2026-05-26 21:42:23 -0300

    feat: corrigindo erros de catalogoController e catalogoService

--- src/main/java/com/SerratecFlix/trabalhoApi/Service/AvaliacaoFilmeService.java
+++ src/main/java/com/SerratecFlix/trabalhoApi/Service/AvaliacaoFilmeService.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.SerratecFlix.trabalhoApi.Domain.Filme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
