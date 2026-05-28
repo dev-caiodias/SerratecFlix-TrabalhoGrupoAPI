ccfe5569efab3fe39f3539ae6eaa98b28d76523f
dev-caiodias <dev.caiodias19@gmail.com>
2026-05-27 21:54:47 -0300

    FEAT: sistema de envio de email CAIO INDIVIDUAL

--- src/main/java/com/SerratecFlix/trabalhoApi/Controller/UsuarioController.java
+++ src/main/java/com/SerratecFlix/trabalhoApi/Controller/UsuarioController.java
@@ -1,7 +1,11 @@
 package com.SerratecFlix.trabalhoApi.Controller;
 
+import com.SerratecFlix.trabalhoApi.Domain.Filme;
+import com.SerratecFlix.trabalhoApi.Dto.Request.CategoriaDTORequest;
 import com.SerratecFlix.trabalhoApi.Dto.Request.UsuarioDTORequest;
+import com.SerratecFlix.trabalhoApi.Dto.Response.FilmeResponse;
 import com.SerratecFlix.trabalhoApi.Dto.Response.UsuarioDTOResponse;
+import com.SerratecFlix.trabalhoApi.Service.RecomendacaoService;
 import com.SerratecFlix.trabalhoApi.Service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -23,6 +27,9 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private RecomendacaoService recomendacaoService;
 
     @GetMapping
     @Operation(summary = "Lista todos os usuários")
@@ -58,6 +65,26 @@ public class UsuarioController {
     @ApiResponse(responseCode = "404", description = "Usuário não encontrado.")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         usuarioService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    /*CRUD para recomendações de filme - parte do Vitor */
+    @GetMapping("/{id}/recomendacoes")
+    public ResponseEntity<List<FilmeResponse>> obterRecomendacoes(@PathVariable Long id, @RequestParam(required = false) Double notaMinima) {
+        List<FilmeResponse> recomendacoes = recomendacaoService.obterRecomendacoes(id, notaMinima);
+        return ResponseEntity.ok(recomendacoes);
+    }
+
+    /*POST */
+    @PostMapping("/{id}/preferencias")
+    public ResponseEntity<Void> salvarPreferencia(@PathVariable Long id, @RequestBody CategoriaDTORequest dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /*DELETE */
+    @DeleteMapping("/{id}/preferencias/{categoriaId}")
+    public ResponseEntity<Void> deletarPreferencia(@PathVariable Long id, @PathVariable Long categoriaId) {
+        recomendacaoService.removerPreferenciaUsuario(id, categoriaId);
         return ResponseEntity.noContent().build();
     }
 }
